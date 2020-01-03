c55122822db90bb0b188435ab95b30c291d7c75e
navnittiwari <42030960+navnittiwari@users.noreply.github.com>
2020-01-03 08:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   });
 }
 
+document.addEventListener("keydown", function(){
+  alert("key was pressed");
+});
 
 // var audio=new Audio('sounds/tom-1.mp3');
 //         audio.play();
