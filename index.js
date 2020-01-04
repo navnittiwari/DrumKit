dccaf742009ff4b5dd43e52cf7d0d522591a25b8
navnittiwari <42030960+navnittiwari@users.noreply.github.com>
2020-01-04 07:48:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 //detecting keyboard press
 document.addEventListener("keydown", function(event){
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -56,5 +60,12 @@ function makeSound(key){
   }
 }
 
-// var audio=new Audio('sounds/tom-1.mp3');
-//         audio.play();
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
